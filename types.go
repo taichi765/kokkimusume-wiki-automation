ae826f7eb5c84985b867b849190ee46caa1e3723
taichi765 <taichi0209.youtub@gmail.com>
2026-08-09 16:01:05 +0900

    Add data

--- types.go
+++ types.go
@@ -1,14 +1,17 @@
 package main
 
+// Request for `POST https://api.wikiwiki.jp/<wiki-name>/auth`
 type AuthRequest struct {
 	Password string `json:"password"`
 }
 
+// Response for `POST https://api.wikiwiki.jp/<wiki-name>/auth`
 type AuthResponse struct {
 	Status string `json:"status"`
 	Token  string `json:"token"`
 }
 
+// Response for `GET https://api.wikiwiki.jp/<wiki-name>/page/<page-name>`
 type GetPageResponse struct {
 	Page      string `json:"page"`
 	Source    string `json:"source"`
