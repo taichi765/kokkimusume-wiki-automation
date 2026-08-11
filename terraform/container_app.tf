a2f4a455c9486a10c845ff2bd47e1d8dc0200b8d
taichi765 <taichi0209.youtub@gmail.com>
2026-08-12 00:38:53 +0900

    chore(terraform): Add managed identity authentication

--- terraform/container_app.tf
+++ terraform/container_app.tf
@@ -22,12 +22,39 @@ resource "azurerm_container_app" "kokkimusume-discordbot" {
   resource_group_name          = azurerm_resource_group.kokkimusume-discordbot.name
   revision_mode                = "Single"
 
+  identity {
+    type = "SystemAssigned"
+  }
+
+  registry {
+    server = azurerm_container_registry.acr.login_server
+    identity = "system"
+  }
+
+  ingress {
+    target_port = 80
+    external_enabled = true
+    traffic_weight {
+      percentage = 100
+      latest_revision = true
+    }
+  }
+
   template {
+    min_replicas = 0
+    max_replicas = 1
+
     container {
       name   = "kokkimusume-discordbot-container"
-      image  =  "mcr.microsoft.com/k8se/quickstart:latest"#"kokkimusumediscordbot-a7g8a0bnajh6fghc.azurecr.io/kokkimusumediscordbot:v0.0.0"
+      image  =  "${azurerm_container_registry.acr.login_server}/kokkimusumediscordbot:v0.0.0"
       cpu    = 0.25
       memory = "0.5Gi"
     }
   }
+}
+
+resource "azurerm_role_assignment" "acr_pull" {
+  scope = azurerm_container_registry.acr.id
+  role_definition_name = "AcrPull"
+  principal_id = azurerm_container_app.kokkimusume-discordbot.identity[0].principal_id
 }
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -13,3 +13,7 @@ output "azure_tenant_id"{
 output "azure_subscription_id" {
   value = data.azurerm_client_config.current.subscription_id
 }
+
+output "acr-login-server" {
+  value = azurerm_container_registry.acr.login_server
+}
