04e06b9b94761b1dde75add35155352a00fed325
taichi765 <taichi0209.youtub@gmail.com>
2026-08-11 15:31:00 +0900

    flake.nixを追加
    Azure CLIをローカルに入れたくない

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = {self,nixpkgs}: 
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system;};
+    in {
+      devShells.${system}.default =  pkgs.mkShell {
+        buildInputs = [
+          pkgs.go
+          pkgs.azure-cli
+        ];
+      };
+  };
+}
