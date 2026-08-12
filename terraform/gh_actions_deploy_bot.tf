8c7fc5513b979c60ffb423fe76717adb27ed01b0
taichi765 <taichi0209.youtub@gmail.com>
2026-08-12 23:46:32 +0900

    fix(terraform): Own gh-actions-deploy-bot by gh-actions-apply

--- terraform/gh_actions_deploy_bot.tf
+++ terraform/gh_actions_deploy_bot.tf
@@ -1,6 +1,7 @@
 
 resource "azuread_application" "github-actions" {
   display_name = "kokkimusume-discordbot-github-actions"
+  owners       = [azuread_application.gh-actions-apply.object_id]
 }
 
 resource "azuread_service_principal" "github-actions" {
@@ -9,15 +10,15 @@ resource "azuread_service_principal" "github-actions" {
 
 resource "azuread_application_federated_identity_credential" "github-actions" {
   application_id = azuread_application.github-actions.id
-  display_name = "kokkimusume-discordbot-github-actions-cred"
-  description = "Github Actions OIDC federation"
-  audiences = ["api://AzureADTokenExchange"]
-  issuer = "https://token.actions.githubusercontent.com"
-  subject = "repo:taichi765@190380265/kokkimusume-wiki-automation@1328476148:ref:refs/heads/master"
+  display_name   = "kokkimusume-discordbot-github-actions-cred"
+  description    = "Github Actions OIDC federation"
+  audiences      = ["api://AzureADTokenExchange"]
+  issuer         = "https://token.actions.githubusercontent.com"
+  subject        = "repo:taichi765@190380265/kokkimusume-wiki-automation@1328476148:ref:refs/heads/master"
 }
 
 resource "azurerm_role_assignment" "acr_push" {
-  scope = azurerm_container_registry.acr.id
+  scope                = azurerm_container_registry.acr.id
   role_definition_name = "AcrPush"
-  principal_id = azuread_service_principal.github-actions.object_id
+  principal_id         = azuread_service_principal.github-actions.object_id
 }
