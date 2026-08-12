782ae78c76148e09a0ecc81907aeb8ad26940453
taichi765 <taichi0209.youtub@gmail.com>
2026-08-13 00:03:20 +0900

    fmt

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,9 +4,9 @@ resource "azurerm_resource_group" "kokkimusume-discordbot" {
 }
 
 resource "azurerm_container_registry" "acr" {
-  name = "kokkimusumeDiscordbotRegistry"
+  name                = "kokkimusumeDiscordbotRegistry"
   resource_group_name = azurerm_resource_group.kokkimusume-discordbot.name
-  location = azurerm_resource_group.kokkimusume-discordbot.location
-  sku = "Standard"
-  admin_enabled = false
+  location            = azurerm_resource_group.kokkimusume-discordbot.location
+  sku                 = "Standard"
+  admin_enabled       = false
 }
