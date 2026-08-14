8d47a56862925057960e0589b8d80392b2bf370f
taichi765 <taichi0209.youtub@gmail.com>
2026-08-14 17:48:38 +0900

    ci: Refactor CI/CD

--- terraform/main/outputs.tf
+++ terraform/main/outputs.tf
@@ -13,3 +13,12 @@ output "azure_subscription_id" {
 output "acr-login-server" {
   value = azurerm_container_registry.acr.login_server
 }
+
+output "acr_deploy_client_id" {
+  value = azuread_application.acr-deploy.client_id
+}
+
+output "latest_image_tag" {
+  # 将来的にprodとdevで分けたらここも分岐する
+  value = var.commit_sha256
+}
